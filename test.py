155867fcf6228ec54aa50f122ebe789a8ea342cc
thopeubey <thomas.peubey@partner.auchan.com>
2021-10-01 10:35:06 +0200

    nouveau test

--- test.py
+++ test.py
@@ -1,4 +1,31 @@
 import unittest
 
 
-class MyFirstTest(unittest.TestCase)
+def add(x, y):
+    return x + y
+
+
+def substract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    next
+
+
+class MyFirstTest(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 2), 4)
+
+    def test_substract(self):
+        self.assertEqual(substract(2, 2), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
